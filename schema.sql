c05293bfdf21ebcd072e06ca77d0e2c34d6717fa
Max Fitton <maxfitton@gmail.com>
2016-04-30 16:52:14 -0700

    Updates to schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
-CREATE TABLE Users (
-	uid INT NOT NULL PRIMARY KEY,
+CREATE TABLE Clients (
+	cid INT NOT NULL PRIMARY KEY,
 	name TEXT,
 	age INT,
 	city TEXT,
@@ -11,4 +11,32 @@ CREATE TABLE Users (
 )
 CREATE TABLE EmergencyContacts (
 	ecid INT NOT NULL PRIMARY KEY,
+	name TEXT,
+	phone_number TEXT
+)
+
+CREATE TABLE ServiceRequests (
+	rid INT NOT NULL PRIMARY KEY,
+	cid INT REFERENCES Clients,
+	attendant_id INT REFERENCES Employees,
+	service_type INT REFERENCES Services,
+	latitude INT,
+	longitude INT,
+	start_time DATETIME,
+	end_time DATETIME,
+	dispatch_notes TEXT,
+	attendant_notes TEXT
+)
+
+CREATE TABLE Equipment (
+	eid INT NOT NULL PRIMARY KEY,
+	equipment_type REFERENCES EquipmentTypes,
+	in_use INT,
+	borrower_id REFERENCES Clients
+)
+
+CREATE TABLE EquipmentTypes (
+	etid INT NOT NULL PRIMARY KEY,
+	name TEXT,
+	description TEXT
 )
